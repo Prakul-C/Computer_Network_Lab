9a9c6086c33ac275ae4c360ed6657538c9284f82
Prakul C <prakulc6@gmail.com>
2026-02-02 22:56:10 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,5 +1,3 @@
-//Write a program for error detecting code using CRC-CCITT(16-bits)
-
 import java.util.Scanner;
 
 public class CRC {
@@ -9,7 +7,6 @@ public class CRC {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("At Sender Side: ");
-        // Input Data Stream
         System.out.print("Enter message bits: ");
         String message = sc.nextLine();
         System.out.print("Enter generator: ");
@@ -24,26 +21,21 @@ public class CRC {
         for (int i = 0; i < generator.length(); i++)
             divisor[i] = Integer.parseInt(generator.charAt(i) + "");
 
-        // Calculation of CRC
         for (int i = 0; i < message.length(); i++) {
             if (data[i] == 1) {
                 for (int j = 0; j < divisor.length; j++)
                     data[i + j] ^= divisor[j];
             }
         }
-
-        // Append the remainder to the original message
         for (int i = 0; i < message.length(); i++)
             data[i] = Integer.parseInt(message.charAt(i) + "");
 
-        // Display CRC
         System.out.print("The checksum code is: ");
         for (int i = 0; i < data.length; i++)
             System.out.print(data[i]);
         System.out.println();
 
         System.out.println("At Receiver Side: ");
-        // Check for input CRC code
         System.out.print("Enter checksum code: ");
         message = sc.nextLine();
         System.out.print("Enter generator: ");
@@ -58,14 +50,12 @@ public class CRC {
         for (int i = 0; i < generator.length(); i++)
             divisor[i] = Integer.parseInt(generator.charAt(i) + "");
 
-        // Calculation of remainder
         for (int i = 0; i < message.length(); i++) {
             if (data[i] == 1)
                 for (int j = 0; j < divisor.length; j++)
                     data[i + j] ^= divisor[j];
         }
 
-        // Display validity of data
         boolean valid = true;
         for (int i = 0; i < data.length; i++)
             if (data[i] == 1) {
@@ -79,8 +69,6 @@ public class CRC {
             System.out.println("Data stream is invalid. CRC error occurred.");
     }
 }
-
-// fix the generator same at both sender and receiver side
 
 // output:-
 // At Sender Side: 
